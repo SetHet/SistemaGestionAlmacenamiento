d55a0b41b5ef8905d12361acd383a913823a64b0
SetHet <36393893+SetHet@users.noreply.github.com>
2020-11-26 02:15:36 -0300

    Agregado Configuracion Negocio SGA

---
+++ NegocioSGA/ConfiguracionNegocioSGA.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NegocioSGA
+{
+    public class ConfiguracionNegocioSGA
+    {
+        private const string server = "DESKTOP-F8TM6K2";
+        private const string dataBase = "SGA";
+
+        public static string Server { get { return server; } }
+
+        public static string DataBase { get { return dataBase; } }
+    }
+}
