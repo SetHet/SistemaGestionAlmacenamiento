59c1b9e4c07524e1647e08d0ecc1e26c553a103d
SetHet <36393893+SetHet@users.noreply.github.com>
2020-11-26 22:17:50 -0300

    Update NegocioEmpleado.cs

--- NegocioSGA/NegocioEmpleado.cs
+++ NegocioSGA/NegocioEmpleado.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using ModeloDatosSGA;
 using ConexionBD;
+using System.Data;
 
 namespace NegocioSGA
 {
@@ -30,10 +31,25 @@ namespace NegocioSGA
 
         public MDEmpleado Login(string rut, string password)
         {
+            string query = $"SELECT TOP (1) * FROM Empleado WHERE rut_empleado = '{rut}' AND password = '{password}'";
 
+            DataTable table = Conexion.Select(query);
 
+            if (table != null && table.Rows.Count > 0)
+            {
+                NewEmpleado();
 
-            return null;
+                Empleado.Rut = table.Rows[0]["rut_empleado"].ToString();
+                Empleado.Nombre = table.Rows[0]["nombre"].ToString();
+                Empleado.Apellido = table.Rows[0]["apellido"].ToString();
+                Empleado.Password = table.Rows[0]["password"].ToString();
+
+                return Empleado;
+            }
+            else
+            {
+                return null;
+            }
         }
 
     }
