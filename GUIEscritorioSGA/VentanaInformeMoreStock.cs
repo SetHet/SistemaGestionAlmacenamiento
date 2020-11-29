0e120f1a02d10282043454e795b682323b33c8ca
SetHet <36393893+SetHet@users.noreply.github.com>
2020-11-29 15:52:18 -0300

    Algunas ventanas Informe

---
+++ GUIEscritorioSGA/VentanaInformeMoreStock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUIEscritorioSGA
+{
+    public partial class VentanaInformeMoreStock : Form
+    {
+        public VentanaInformeMoreStock()
+        {
+            InitializeComponent();
+        }
+    }
+}
