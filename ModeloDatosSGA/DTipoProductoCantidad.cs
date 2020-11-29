4695c88e2bbe854fe1aef8b3070f63a56c9d104c
SetHet <36393893+SetHet@users.noreply.github.com>
2020-11-29 19:19:54 -0300

    Negocio TipoProducto ByBodega

---
+++ ModeloDatosSGA/DTipoProductoCantidad.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloDatosSGA
+{
+    public class DTipoProductoCantidad
+    {
+        private int id_tipo_producto;
+        private int cantidad;
+
+        public int Id_tipo_producto { get => id_tipo_producto; set => id_tipo_producto = value; }
+        public int Cantidad { get => cantidad; set => cantidad = value; }
+    }
+}
